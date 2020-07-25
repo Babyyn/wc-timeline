9daec958e22a8136a122d7187cc74a88c0398ea3
Chunfu Cai <chunfu.cai@thoughtworks.com>
2020-07-25 19:20:28 +0800

    [ccf]add network request with interceptor.

--- timeline/src/main/java/com/ccf/wc/timeline/core/TimelineAppModule.kt
+++ timeline/src/main/java/com/ccf/wc/timeline/core/TimelineAppModule.kt
@@ -1,9 +1,23 @@
 package com.ccf.wc.timeline.core
 
+import android.util.Log
 import com.ccf.wc.baselib.BaseAppModule
 
 class TimelineAppModule : BaseAppModule() {
 
+    private val TAG = "TimelineAppModule"
+
     override fun onCreate() {
+        super.onCreate()
+        // TODO
+    }
+
+    override fun lazyInitAfterLaunched() {
+        super.lazyInitAfterLaunched()
+        // TODO
+    }
+
+    override fun doNotIncludeWhenLaunch(): Boolean {
+        return true
     }
 }
